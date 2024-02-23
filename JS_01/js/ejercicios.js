065b64c8a28fcdba0885cc4420d84df8d18da796
Lorena <lorenapeironavarro@gmail.com>
2024-02-23 12:05:40 +0100

    Empezamos Javascript

---
+++ JS_01/js/ejercicios.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt ("Indicame el primer numero a sumar"))
+let num2 = parseInt(prompt ("Indicame el segundo numero a sumar"))
+console.log(typeof(num1))
+alert ("El total es " + (num1 + num2))
